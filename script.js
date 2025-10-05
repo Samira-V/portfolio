cb1fbb81d11f50b2ee2bcafb47f81e64f7b13be1
samira vaeidi <samiraa.vaeidi@gmail.com>
2025-10-05 08:56:07 +0330

    update submenu

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// const panel = document.querySelector('.panel')
 const accordion = document.querySelectorAll('.accordion')
 const accordionFaq = document.querySelectorAll('.accordion-faq')
 const arrow = document.querySelector('.arrow')
@@ -8,24 +7,14 @@ const projects = document.querySelectorAll(".project")
 console.log(arrow)
 for (i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", function () {
-
         this.classList.toggle("active");
-
         this.style.backgroundColor = "var(--black)";
         this.style.color = "var(--lightGray)";
-        // accordionSvg.style.backgroundColor = "var(--orenge)"
-        // accordionSvg.style.fill = "#fff"
-        // this.style.padding = "10px";
-
         let panel = this.nextElementSibling;
         if (panel.style.maxHeight === "500px") {
             panel.style.maxHeight = "0";
             this.style.backgroundColor = "var(--lightGray)";
             this.style.color = "var(--black)";
-
-
-            // panel.style.backgroundColor = "var(--black)"
-            // panel.style.color = "var(--lightGray)"
 
         } else {
             panel.style.maxHeight = "500px";
@@ -35,28 +24,20 @@ for (i = 0; i < accordion.length; i++) {
 }
 for (i = 0; i < accordionFaq.length; i++) {
     accordionFaq[i].addEventListener("click", function () {
-
+        console.log(accordionFaq)
         this.classList.toggle("active");
-
         this.style.backgroundColor = "var(--orenge)";
         this.style.color = "var(--lightGray)";
         this.style.borderRadius = " 20px 20px 0 0"
         this.style.border = "none"
-        accordionSvg.style.backgroundColor = "var(--orenge)"
-        accordionSvg.style.color = "#fff"
-        // this.style.padding = "10px";
-
+        // accordionSvg.style.backgroundColor = "var(--orenge)"
+        // accordionSvg.style.color = "#fff"
         let panel = this.nextElementSibling;
         if (panel.style.maxHeight === "500px") {
             panel.style.maxHeight = "0";
             panel.style.backgroundColor = "var(--orenge)"
             this.style.backgroundColor = "var(--lightGray)";
-
             this.style.color = "var(--black)";
-
-
-            // panel.style.backgroundColor = "var(--black)"
-            // panel.style.color = "var(--lightGray)"
 
         } else {
             panel.style.maxHeight = "500px";
@@ -66,13 +47,13 @@ for (i = 0; i < accordionFaq.length; i++) {
 }
 
 
-const handelClick = (index)=>{
-projects.forEach((project , i) => {
-if(index == i){
-    images[i].classList.add("img-active")
-    
-}else{
-    images[i].classList.remove("img-active")
-}
-})
+const handelHover = (index) => {
+    projects.forEach((project, i) => {
+        if (index == i) {
+            images[i].classList.add("img-active")
+
+        } else {
+            images[i].classList.remove("img-active")
+        }
+    })
 }
