a083789556a8b8dcc51319059d5e49f06db95b27
samira vaeidi <samiraa.vaeidi@gmail.com>
2025-10-09 18:52:47 +0330

    update map

--- connect.js
+++ connect.js
@@ -1,5 +1,20 @@
-var map = L.map('map').setView([35, 50], 16);
+var map = L.map('map').setView([38.29, 45.95], 4);
+var marker = L.marker([38.29, 45.95]).addTo(map);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+
+marker.bindPopup("موقعیت").openPopup();
+
+
+
+
+function onMapClick(e) {
+    alert(" شما در این موقعیت مکانی کلیک کردید " + e.latlng);
+   marker= L.marker([e.latlng, 45.95]).addTo(map);
+    
+}
+map.on('click', onMapClick);
